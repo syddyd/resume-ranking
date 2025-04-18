6be78106288b5132ff468a94e9c2ffcb532d340b
syddyd <sydney.dacks@gmail.com>
2025-04-18 15:39:22 -0400

    preprocessing and scaling'

--- ranking-resumes/main.py
+++ ranking-resumes/main.py
@@ -4,23 +4,35 @@ import tensorflow_ranking as tfr
 import numpy as np 
 import pandas as pd
 from aif360.datasets import BinaryLabelDataset
+from sklearn.preprocessing import StandardScaler
 
 # Suppose you want to protect 'race' and 'gender' at once:
 # You must provide them in the features or as separate attributes
-fairCV = np.load("./data/FairCVdb.npy", allow_pickle = True).item()
+fairCV = np.load("./FairCVdb.npy", allow_pickle = True).item()
 ds = fairCV['Profiles Train']
 dy = fairCV['Biased Labels Train (Gender)']
-print(ds.shape)
-ds = np.delete(ds, np.s_[12:51], axis=1)
-np.random.shuffle(ds)
-print(ds.shape)
-ds = np.reshape(ds, (-1, 1, ds.shape[1]))
-#print(ds.shape)
 
 ds_test = fairCV['Profiles Test']
+
+#get rid of the facial encodings and shuffle
+ds = np.delete(ds, np.s_[12:51], axis=1)
+np.random.shuffle(ds)
+ds = np.reshape(ds, (-1, 1, ds.shape[1]))
+
 ds_test = np.delete(ds_test, np.s_[12:51], axis=1)
+np.random.shuffle(ds_test)
 ds_test = np.reshape(ds_test, (-1, 1, ds_test.shape[1]))
-dy_test = fairCV['Blind Labels Test']
+
+#get rid of data labels 
+df = ds.squeeze()  # shape: (n_samples, n_features)
+dt = ds_test.squeeze()
+
+labels = np.array(dy).reshape(-1, 1)  # shape: (n_samples, 1)
+
+#use sklearn standard scaler to scale the data 
+scaler = StandardScaler() 
+scaledData = scaler.fit_transform(X=df)
+scaledTestData = scaler.transform(X=dt)
 
 # Assume ds contains features, dy are binary labels, group_ids contain race, gender, etc.
 
@@ -28,8 +40,8 @@ features = ds.squeeze()  # shape: (n_samples, n_features)
 labels = np.array(dy).reshape(-1, 1)  # shape: (n_samples, 1)
 df = pd.DataFrame(features)
 df['label'] = labels
-df['race'] = race_column  # supply actual values
-df['gender'] = gender_column  # supply actual values
+df['race'] = fairCV[:0]  # supply actual values
+df['gender'] = fairCV[:1]  # supply actual values
 
 # Define privileged/unprivileged groups
 privileged_groups = [{'race': 1, 'gender': 1}]  # e.g., white male
